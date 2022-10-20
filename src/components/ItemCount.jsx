0e4eeb1aa30e0b34ab133670dccb79f3f5d5a34e
chiky222 <cristian_la22@hotmail.com>
2022-10-20 00:06:36 -0300

    Actividad N° 1 - Contador con Botón

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ImPlus, ImMinus } from 'react-icons/im';
+import { useState } from 'react';
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  
+  const [count, setCount] = useState(0);
+  
+  const sumar = () => {
+    if(count < stock){
+        setCount(count + 1);
+    };
+  };
+  
+  const restar = () => {
+    if(count > 0){
+        setCount(count - 1);
+    };
+  };
+
+  return (
+    <>
+        <div className='visor'>
+            <ImPlus className='plusIcon' onClick={sumar} />
+            <p className='contador' >{count}</p>
+            <ImMinus className='minusIcon' onClick={restar} />
+            <button className='buttonAdd' onClick={() => onAdd(count)} disabled={count === 0} >Agregar</button>
+        </div>
+    </>
+  )
+};
+
+export default ItemCount;
