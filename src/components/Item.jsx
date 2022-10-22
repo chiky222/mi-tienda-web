eeeac219fad9dd116f7d0cc180613e928296cbae
chiky222 <cristian_la22@hotmail.com>
2022-10-22 03:02:01 -0300

    Cambio de Estilo de Imágenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,7 +7,7 @@ const Item = ({producto}) => {
         <img src={producto.img} width='300px' alt={producto.title} />
         <article>
             <h2>{producto.title}</h2>
-            <h3>${producto.price}</h3>
+            <h3>$ {producto.price}</h3>
             <ItemCount stock={producto.stock} initial={1} onAdd={(count) => console.log(count)} />
         </article>
     </div>
