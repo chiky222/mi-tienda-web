2a0e385f35c06c1e89ac071510cbc3d8e4b24fcd
chiky222 <cristian_la22@hotmail.com>
2022-11-10 23:40:12 -0300

    Firebase

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -9,13 +9,12 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 const ItemListContainer = ({titulo}) => {
 
   const [items, setItems] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   const { categoryName } = useParams();
 
   useEffect(() => {
 
     const collectionProd = collection(baseDeDatos, 'productos');
-    //const queryCategory = query(collectionProd, where('category', '==', categoryName));
 
     if (categoryName === undefined) {
       getDocs(collectionProd)
@@ -31,6 +30,9 @@ const ItemListContainer = ({titulo}) => {
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
       })
 
       // const getProductos = (categoryName) => {   HACER CONDICIONAL PARA CATEGORIAS O TODO      
@@ -66,20 +68,28 @@ const ItemListContainer = ({titulo}) => {
       .catch((error) => {
         console.log(error);
       })
+      .finally(() => {
+        setLoading(false);
+      })
     }
 
   }, [categoryName]);
 
-    
+  if (loading) {
+    return (<div className='contenedor-detalle-items'>
+              <h1>Cargando...</h1>
+            </div>)
+  } else {
+    return (
+      <>
+          <h1 className='titulo-saludo'>{titulo}</h1>
+          <h4 className='subtitulo-pie'>LLevate uno de los NFT Edición Limitada de Lionel Messi, de imágenes generadas con Inteligencia Artificial.<br></br>Para crearlas se utilizaron tecnologías como Stable Diffusion y OpenAi Dall-E 2.<br></br><br></br></h4>
+          <ItemList items={items} />
+      </>
+    )
+  }
 
-
-  return (
-    <>
-        <h1 className='titulo-saludo'>{titulo}</h1>
-        <h4 className='subtitulo-pie'>LLevate uno de los NFT Edición Limitada de Lionel Messi, de imágenes generadas con Inteligencia Artificial.<br></br>Para crearlas se utilizaron tecnologías como Stable Diffusion y OpenAi Dall-E 2.<br></br><br></br></h4>
-        <ItemList items={items} />
-    </>
-  )
+  
 };
 
 export default ItemListContainer; 
